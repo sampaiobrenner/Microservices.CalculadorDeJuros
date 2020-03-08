dfde0ba0087a5f5df5a4fb047be7bb9420f2e218
Matheus Sampaio <sampaio.brenner@gmail.com>
2020-03-08 13:14:37 -0300

    CM - Criando controllers

---
+++ Microservices.CalculadorDeJuros.WebApi/Controllers/v1/CalculadorDeJurosV1Controller.cs
@@ -0,0 +1,10 @@
+﻿using Microservices.CalculadorDeJuros.WebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.CalculadorDeJuros.WebApi.Controllers.v1
+{
+    [ApiVersion("1")]
+    public class CalculadorDeJurosV1Controller : BaseController
+    {
+    }
+}
